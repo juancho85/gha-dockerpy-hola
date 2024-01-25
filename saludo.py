6af925fb4a991350c2666f86a4338fa543360876
Juan David Sánchez <juancho85@gmail.com>
2024-01-25 13:15:08 +0100

    init repo

---
+++ saludo.py
@@ -0,0 +1,13 @@
+import os
+
+def set_github_action_output(output_name, output_value):
+    f = open(os.path.abspath(os.environ["GITHUB_OUTPUT"]), "a")
+    f.write(f'{output_name}={output_value}')
+    f.close()
+
+def run():
+    nombre = os.getenv('INPUT_nombre', default="John Doe")
+    set_github_action_output('saludo', f'Hola, {nombre}!')
+
+if __name__ == '__main__':
+    run()
